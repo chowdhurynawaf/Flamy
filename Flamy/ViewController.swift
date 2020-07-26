5c05a36094b99309b75a6c52ae6724c74bd9455c
nawafChowdhuryAkash <nawafc959@gmail.com>
2020-07-27 01:45:40 +0600

    project-intialised

---
+++ Flamy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flamy
+//
+//  Created by as on 7/27/20.
+//  Copyright © 2020 nawaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
